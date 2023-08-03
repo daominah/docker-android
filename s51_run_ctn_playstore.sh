9e9f6fdba5e5b2f088f7f069cdbf72774e68e75f
Đào Thanh Tùng <tung.dao@difisoft.com>
2023-08-03 17:03:41 +0700

    add optionMount to docker run

--- s51_run_ctn_playstore.sh
+++ s51_run_ctn_playstore.sh
@@ -2,11 +2,12 @@ set -x
 
 export dockerImgTag=daominah/docker-android11-playstore
 export dockerCtnName=android-container-playstore
+export optionMount='-v /media/tungdt/LinuxData/work/TuanNghiep/apk_installer:/root/tmp'
 
 docker rm -f ${dockerCtnName}
-docker run --privileged -d \
-  -p 6080:6080 -p 5554:5554 -p 5555:5555 -p 4723:4723 \
-  -e DEVICE="Samsung Galaxy S10" -e APPIUM=true \
-  --name ${dockerCtnName} ${dockerImgTag}
+docker run --privileged -d $optionMount \
+    -p 6080:6080 -p 5554:5554 -p 5555:5555 -p 4723:4723 \
+    -e DEVICE="Samsung Galaxy S10" -e APPIUM=true \
+    --name ${dockerCtnName} ${dockerImgTag}
 
 set +x
